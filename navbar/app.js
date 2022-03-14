9cde3d387a73aaa0a4a2600be8c9fdab8230e4f1
ilanlavi <refaelberlin@gmail.com>
2022-03-14 23:00:24 +0100

    projects

--- navbar/app.js
+++ navbar/app.js
@@ -3,3 +3,25 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navBtn = document.querySelector(".nav-toggle");
+const navLinks = document.querySelector(".links");
+
+const navToggle = () => {
+    if (navLinks.classList.contains("show-links") ? navLinks.classList.remove("show-links") : navLinks.classList.add("show-links"));
+}
+
+const windowResize = () => {
+    if (window.innerWidth > 800) navLinks.classList.remove("show-links");
+}
+
+navBtn.addEventListener('click', () => {
+    //navLinks.classList.toggle("show-links");
+    navToggle();
+
+});
+
+window.addEventListener('resize', windowResize);
+
+
+
